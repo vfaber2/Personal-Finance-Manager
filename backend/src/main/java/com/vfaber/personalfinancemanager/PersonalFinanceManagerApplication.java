02f9f70eb02a8a72789e1cb6cf5d59822b7d7225
Victor <vfaber@pt.lu>
2024-03-28 16:36:15 +0100

    added spring initializr

---
+++ backend/src/main/java/com/vfaber/personalfinancemanager/PersonalFinanceManagerApplication.java
@@ -0,0 +1,13 @@
+package com.vfaber.personalfinancemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFinanceManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalFinanceManagerApplication.class, args);
+	}
+
+}
