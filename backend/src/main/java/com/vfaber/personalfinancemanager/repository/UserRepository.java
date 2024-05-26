a9889fdde7211d46c382416426c738d29f3b0e35
Victor <vfaber@pt.lu>
2024-05-26 14:49:30 +0200

    added findAll

--- backend/src/main/java/com/vfaber/personalfinancemanager/repository/UserRepository.java
+++ backend/src/main/java/com/vfaber/personalfinancemanager/repository/UserRepository.java
@@ -2,11 +2,14 @@ package com.vfaber.personalFinanceManager.repository;
 
 
 import com.vfaber.personalFinanceManager.entity.UserEntity;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
-public interface UserRepository extends CrudRepository<UserEntity, String> {
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
     Optional<UserEntity> findByUsername(String username);
+
+    List<UserEntity> findAll();
 }
