5f17b30c6046edf2c46d91c2b66b6d140fb301e9
Victor <vfaber@pt.lu>
2025-01-21 12:02:49 +0100

    updated config for h2 db + added basic user for initialization

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO User_Entity (username, password, first_name, last_name, email)
+VALUES ('user1', 'password123', 'John', 'Doe', 'john.doe@gmail.com'),
+       ('user2', 'password456', 'Jane', 'Smith', 'jane.smith@gmail.com'),
+       ('user3', 'password789', 'Alice', 'Johnson', 'alice.johnson@gmail.com');
